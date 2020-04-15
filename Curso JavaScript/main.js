fab0c67921aa4fd74e718cdc4865cb1de469913e
JVidalN <j.vidalnunes@gmail.com>
2020-04-15 12:17:31 -0300

    [Curso Javascript] Use Axios

--- Curso JavaScript/main.js
+++ Curso JavaScript/main.js
@@ -9,28 +9,36 @@
 //     }
 // }
 
-var minhaPromise = function(){
-    return new Promise(function(resolve, reject){
-        var xhr = new XMLHttpRequest();
-        xhr.open('GET','https://api.github.com/users/jvidaln');
-        xhr.send(null);
+// var minhaPromise = function(){
+//     return new Promise(function(resolve, reject){
+//         var xhr = new XMLHttpRequest();
+//         xhr.open('GET','https://api.github.com/users/jvidaln');
+//         xhr.send(null);
 
-        xhr.onreadystatechange = function(){
-            if(xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    resolve(JSON.parse(xhr.responseText));
-                } else {
-                    reject('Erro na requisição');
-                }
-            }
-        }
-    });
-}
+//         xhr.onreadystatechange = function(){
+//             if(xhr.readyState === 4) {
+//                 if (xhr.status === 200) {
+//                     resolve(JSON.parse(xhr.responseText));
+//                 } else {
+//                     reject('Erro na requisição');
+//                 }
+//             }
+//         }
+//     });
+// }
 
- minhaPromise()
-    .then(function(response) { 
+//  minhaPromise()
+//     .then(function(response) { 
+//         console.log(response);
+//     })
+//     .catch(function (error) { 
+//         console.warn(error);
+//     })
+
+axios.get('https://api.github.com/users/jvidaln')
+    .then(function (response) {
         console.log(response);
     })
-    .catch(function (error) { 
+    .catch(function (error) {
         console.warn(error);
     })
