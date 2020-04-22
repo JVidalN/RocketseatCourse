1ed35cc47b465592e6d487b1a5bf507bac145b31
JVidalN <j.vidalnunes@gmail.com>
2020-04-21 21:31:27 -0300

    [Curso ES6] Use axios with Async/Await

--- Curso JavaScript ES6/src/main.js
+++ Curso JavaScript ES6/src/main.js
@@ -318,9 +318,9 @@
 
 // // // Async/Await
 
-const minhaPromise = () => new Promise((resolve, reject) => {
-    setTimeout(() => { resolve('OK') }, 2000);
-});
+// const minhaPromise = () => new Promise((resolve, reject) => {
+//     setTimeout(() => { resolve('OK') }, 2000);
+// });
 
 // minhaPromise()
 //     .then(response => {
@@ -337,10 +337,26 @@ const minhaPromise = () => new Promise((resolve, reject) => {
 
 // executaPromise();
 
-const executaPromise = async () =>{
-    console.log(await minhaPromise());
-    console.log(await minhaPromise());
-    console.log(await minhaPromise());
-};
+// const executaPromise = async () =>{
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+//     console.log(await minhaPromise());
+// };
 
-executaPromise();
+// executaPromise();
+
+// // // Axios
+import axios from 'axios';
+
+class Api {
+    static async getUserInfo(userName){
+        try{
+            const response = await axios.get(`https://api.github.com/users/${userName}`);
+            console.log(response);
+        } catch(err) {
+            console.warn('Erro na API');
+        }
+    }
+}
+
+Api.getUserInfo('jvidaln');
